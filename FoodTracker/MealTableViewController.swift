d6358ab2241473077266def5c71e0d293d2f88e8
klepek42 <edgarklepek@gmail.com>
2017-02-26 13:11:45 +0100

    Working with Table Views - Persist Data
    Chapter 9 / 9

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -21,8 +21,14 @@ class MealTableViewController: UITableViewController {
         // Use the edit button item provided by the table view controller
         navigationItem.leftBarButtonItem = editButtonItem
         
-        // Load the sample data
-        loadSampleMeals()
+        // Load any saved meals, otherwise load sample data
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        }
+        else {
+            // Load the sample data
+            loadSampleMeals()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,25 +78,12 @@ class MealTableViewController: UITableViewController {
             // Delete the row from the data source
             meals.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
+            saveMeals()
+        }
+        else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     
     // MARK: - Navigation
@@ -125,24 +118,27 @@ class MealTableViewController: UITableViewController {
     
     //MARK: Actions
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
-        if let sourceViewController = sender.source as?
-            MealViewController, let meal = sourceViewController.meal {
+        if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
             
-                if let selectedIndexPath = tableView.indexPathForSelectedRow {
-                    
-                    // Update an existing meal.
-                    meals[selectedIndexPath.row] = meal
-                    tableView.reloadRows(at: [selectedIndexPath], with: .none)
-                }
-                else {
-            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                
+                // Update an existing meal.
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }
+            else {
+                
                 // Add a new meal
                 let newIndexPath = IndexPath(row: meals.count, section: 0)
             
                 meals.append(meal)
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
-                }
             }
+            
+            // Save the meals.
+            saveMeals()
+            
+        }
     }
     
     //MARK: Private Methods
@@ -167,4 +163,21 @@ class MealTableViewController: UITableViewController {
         meals += [meal1, meal2, meal3]
     }
 
+    private func saveMeals() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+        
+        if isSuccessfulSave {
+            os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
+        }
+        else {
+            os_log("Failed to save meals...", log: OSLog.default, type: .error)
+        }
+    }
+    
+    private func loadMeals() -> [Meal]? {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
+    }
+    
 }
+
+
